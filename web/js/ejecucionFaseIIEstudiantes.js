e137da21a0d6e1a35876b51c01aca692def8b85b
Edwin Molina <edwin.molina@byallgraphic.com>
2018-08-28 00:06:51 -0500

    FORMULARIO ejecucion fase ii estudiantes

---
+++ web/js/ejecucionFaseIIEstudiantes.js
@@ -0,0 +1,34 @@
+$( document ).ready(function(){
+	
+	$( "[id^=btnAddFila]" ).each(function(){
+		
+		$( this ).click(function(){
+			
+			var id = this.id.substr( "btnAddFila".length );
+			$( "#dvSesion"+id ).append( $( "#dvFilaSesion"+id ).clone() );
+			
+			$( "#btnRemoveFila"+id ).css({ display: "" });
+		});
+		
+	});
+	
+	$( "[id^=btnRemoveFila]" ).each(function(){
+		
+		$( this ).click(function(){
+			
+			var id = this.id.substr( "btnRemoveFila".length );
+			var total = $( "[id^=dvFilaSesion"+id+"]"  ).length;
+			
+			if( total > 1 ){
+				
+				if( total == 2 ){
+					$( this ).css({ display: "none" });
+				}
+				
+				$( "[id=dvFilaSesion"+id+"]" ).last().remove()
+				
+			}
+		});
+	});
+	
+});
