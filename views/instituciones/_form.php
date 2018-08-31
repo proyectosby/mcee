432462097976794a038ddefd1f741746a5f25e22
Oscar-DLopez <36933192+Oscar-DLopez@users.noreply.github.com>
2018-08-31 11:20:53 -0500

    se incluye el css de los modal

--- views/instituciones/_form.php
+++ views/instituciones/_form.php
@@ -16,6 +16,10 @@ use yii\widgets\ActiveForm;
 /* @var $this yii\web\View */
 /* @var $model app\models\Instituciones */
 /* @var $form yii\widgets\ActiveForm */
+
+
+$this->registerCssFile("@web/css/modal.css", ['depends' => [\yii\bootstrap\BootstrapAsset::className()]]);
+
 ?>
 
 <div class="instituciones-form">
