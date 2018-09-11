f2d2670793cd3decbf2019aeed10f4aa87d1e94c
Oscar-DLopez <36933192+Oscar-DLopez@users.noreply.github.com>
2018-09-10 20:23:50 -0500

    Cambio plantillas
    Se cambian las plantillas del Crud Para que generen el Crud con Modal

--- vendor/yiisoft/yii2-gii/generators/crud/default/views/index.php
+++ vendor/yiisoft/yii2-gii/generators/crud/default/views/index.php
@@ -2,7 +2,9 @@
 
 use yii\helpers\Inflector;
 use yii\helpers\StringHelper;
-
+use yii\helpers\Url;
+use yii\helpers\Html;
+use yii\bootstrap\Modal;
 /* @var $this yii\web\View */
 /* @var $generator yii\gii\generators\crud\Generator */
 
@@ -13,6 +15,9 @@ echo "<?php\n";
 ?>
 
 use yii\helpers\Html;
+use yii\bootstrap\Modal;
+use yii\helpers\Url;
+
 
 use fedemotta\datatables\DataTables;
 use <?= $generator->indexWidgetType === 'grid' ? "yii\\grid\\GridView" : "yii\\widgets\\ListView" ?>;
@@ -24,17 +29,33 @@ use <?= $generator->indexWidgetType === 'grid' ? "yii\\grid\\GridView" : "yii\\w
 
 $this->title = <?= $generator->generateString(Inflector::pluralize(Inflector::camel2words(StringHelper::basename($generator->modelClass)))) ?>;
 $this->params['breadcrumbs'][] = $this->title;
-?>
+?> 
+
+<h1><?= Html::encode($this->title) ?></h1>
+	<?php
+		Modal::Begin([
+			'header'=>'<h3>NombreCrud</h3>',
+			'id'=>'modal',
+			'size'=>'modal-lg',
+		
+		]);
+		echo "<div id='modalContent'></div>";
+		
+		Modal::end();
+		
+		?>
+
 <div class="<?= Inflector::camel2id(StringHelper::basename($generator->modelClass)) ?>-index">
 
-    <h1><?= "<?= " ?>Html::encode($this->title) ?></h1>
+   
 <?= $generator->enablePjax ? "    <?php Pjax::begin(); ?>\n" : '' ?>
 <?php if(!empty($generator->searchModelClass)): ?>
 <?= "    <?php " . ($generator->indexWidgetType === 'grid' ? "// " : "") ?>echo $this->render('_search', ['model' => $searchModel]); ?>
 <?php endif; ?>
 
     <p>
-        <?= "<?= " ?>Html::a(<?= $generator->generateString('Agregar') ?>, ['create'], ['class' => 'btn btn-success']) ?>
+        <?= "<?= " ?> Html::button('Agregar',['value'=>Url::to(['create']),'class'=>'btn btn-success','id'=>'modalButton']) ?>
+		
     </p>
 
 <?php if ($generator->indexWidgetType === 'grid'): ?>
@@ -98,7 +119,26 @@ if (($tableSchema = $generator->getTableSchema()) === false) {
 }
 ?>
 
-            ['class' => 'yii\grid\ActionColumn'],
+            [
+			'class' => 'yii\grid\ActionColumn',
+			'template'=>'{view}{update}{delete}',
+				'buttons' => [
+				'view' => function ($url, $model) {
+					return Html::a('<span name="detalle" class="glyphicon glyphicon-eye-open" value ="'.$url.'" ></span>', $url, [
+								'title' => Yii::t('app', 'lead-view'),
+					]);
+				},
+
+				'update' => function ($url, $model) {
+					return Html::a('<span name="actualizar" class="glyphicon glyphicon-pencil" value ="'.$url.'"></span>', $url, [
+								'title' => Yii::t('app', 'lead-update'),
+					]);
+				}
+
+			  ],
+			
+			],
+
         ],
     ]); ?>
 <?php else: ?>
