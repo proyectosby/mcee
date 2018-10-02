5a0e57b5bee3403dda098cf2847901eb1031b456
aarias409 <aarias409@gmail.com>
2018-10-01 22:39:21 -0500

    Se genera formularuio para el modulo Seguimeinto de egresados

---
+++ models/SeguimientoEgresados.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "seguimiento_egresados".
+ *
+ * @property int $id
+ * @property string $estrategia_seguimiento
+ * @property int $cantidad_promociones
+ * @property int $cantidad_alumnos_egresados
+ * @property int $cantidad_egresados_estudiso
+ *
+ * @property TipoParametro $cantidadEgresadosEstudiso
+ */
+class SeguimientoEgresados extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'seguimiento_egresados';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['estrategia_seguimiento'], 'string'],
+            [['cantidad_promociones', 'cantidad_alumnos_egresados', 'cantidad_egresados_estudiso'], 'default', 'value' => null],
+            [['cantidad_promociones', 'cantidad_alumnos_egresados', 'cantidad_egresados_estudiso'], 'integer'],
+            [['cantidad_egresados_estudiso'], 'exist', 'skipOnError' => true, 'targetClass' => TipoParametro::className(), 'targetAttribute' => ['cantidad_egresados_estudiso' => 'id']],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'estrategia_seguimiento' => 'Estrategia Seguimiento',
+            'cantidad_promociones' => 'Cantidad Promociones',
+            'cantidad_alumnos_egresados' => 'Cantidad Alumnos Egresados',
+            'cantidad_egresados_estudiso' => 'Cantidad Egresados Estudiso',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCantidadEgresadosEstudiso()
+    {
+        return $this->hasOne(TipoParametro::className(), ['id' => 'cantidad_egresados_estudiso']);
+    }
+}
