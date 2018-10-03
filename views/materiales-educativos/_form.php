9b32a122ec8c9684be249fc8c751da5e2b0275e1
Oscar-DLopez <36933192+Oscar-DLopez@users.noreply.github.com>
2018-10-03 18:30:36 -0500

    Subir Archivos Materiales educativos

--- views/materiales-educativos/_form.php
+++ views/materiales-educativos/_form.php
@@ -6,33 +6,57 @@ use yii\widgets\ActiveForm;
 /* @var $this yii\web\View */
 /* @var $model app\models\MaterialesEducativos */
 /* @var $form yii\widgets\ActiveForm */
-$this->registerCssFile("@web/css/modal.css", ['depends' => [\yii\bootstrap\BootstrapAsset::className()]]);
+$this->registerJsFile(Yii::$app->request->baseUrl.'/js/materialesEducativos.js',['depends' => [\yii\web\JqueryAsset::className()]]);
+$idInstitucion = $_SESSION['instituciones'][0];
+echo Html::hiddenInput( 'idInstitucion', $idInstitucion );
 ?>
 
 <div class="materiales-educativos-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+   <?php $form = ActiveForm::begin([
+		'method' 				=> 'post',
+		'enableClientValidation'=> true,		
+		'options' 				=> [ 'enctype' => 'multipart/form-data' ],
+	]); ?>
 
-    <?= $form->field($model, 'tipo')->DropDownList($tipo,['prompt'=>'Seleccione...']) ?>
-
-    <?= $form->field($model, 'autor')->DropDownList($autor,['prompt'=>'Seleccione...']) ?>
-
-    <?= $form->field($model, 'nivel')->DropDownList($nivel,['prompt'=>'Seleccione...'])?>
+	<div class=table>
+		<div class=row>
+			<div class="form-group" style='display:inline;'>
+				<?= Html::buttonInput('Agregar', ['class' => 'btn btn-success', 'onclick' => 'agregarCampos()', 'id' => 'btnAgregar' ]) ?>
+			</div>
+			
+			<div class="form-group" style='display:inline;'>
+				<?= Html::buttonInput('Eliminar', ['class' => 'btn btn-success', 'onclick' => 'eliminarCampos()', 'id' => 'btnEliminar', 'display' => 'none' ]) ?>
+			</div>
+		</div>
+	</div>
 	
-
-
-    <?= $form->field($model, 'otro_cual')->textInput() ?>
-
-    <?= $form->field($model, 'nombre_apellidos')->textInput() ?>
-
-    <?= $form->field($model, 'reseña')->textArea() ?>
+	<div id=dvTable class=table>
+		<div class=row>
 	
-	<?= $form->field($model, 'estado')->DropDownList($estados) ?>
+			<?= $form->field($model, '[0]tipo')->DropDownList($tipo,['prompt'=>'Seleccione...']) ?>
+			
+			<?= $form->field($model, '[0]otro_cual')->textInput() ?>
 
-    <div class="form-group">
-        <?= Html::submitButton('Guardar', ['class' => 'btn btn-success']) ?>
-    </div>
+			<?= $form->field($model, '[0]autor')->DropDownList($autor,['prompt'=>'Seleccione...']) ?>
 
-    <?php ActiveForm::end(); ?>
+			<?= $form->field($model, '[0]nivel')->DropDownList($nivel,['prompt'=>'Seleccione...'])?>
+			
+			<?= $form->field($model, '[0]nombre_apellidos')->textInput() ?>
 
+			<?= $form->field($model, '[0]reseña')->textArea() ?>
+			
+			<?= $form->field($model, '[0]ruta')->fileInput() ?>
+			
+			<?= $form->field($model, '[0]estado')->DropDownList($estados) ?>
+
+		</div>
+	</div>
+	
+	
+		<div class="form-group">
+			<?= Html::submitButton('Guardar', ['class' => 'btn btn-success']) ?>
+		</div>
+
+		<?php ActiveForm::end(); ?>
 </div>
