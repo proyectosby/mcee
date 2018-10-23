9057775c3c9b7b72cc8725a36b7b022549abbd1e
aarias409 <aarias409@gmail.com>
2018-10-22 22:11:50 -0500

    Se mofica formulario inorme de avance mensual para ser guardo en base de datos

--- models/Ieo.php
+++ models/Ieo.php
@@ -5,33 +5,20 @@ namespace app\models;
 use Yii;
 
 /**
- * This is the model class for table "ieo".
+ * This is the model class for table "ec.ieo".
  *
  * @property int $id
- * @property int $persona_id
  * @property int $institucion_id
  * @property int $sede_id
- * @property int $proyecto_id
- * @property string $estado
- * 
- *
- * @property DocumentosReconocimiento[] $documentosReconocimientos
- * @property Evidencias[] $evidencias
- * @property Instituciones $institucion
- * @property Personas $persona
- * @property ProyectoIeo $proyecto
- * @property Sedes $sede
- * @property Producto[] $productos
- * @property TiposCantidadPoblacion[] $tiposCantidadPoblacions
+ * @property int $estado
+ * @property string $persona_acargo
+ * @property string $codigo_dane
+ * @property string $zona_educativa
+ * @property string $comuna
+ * @property string $barrio
  */
 class Ieo extends \yii\db\ActiveRecord
 {
-
-    public $codigo_dane;
-    public $zona_educativa;
-    public $comuna;
-    public $barrio;
-
     /**
      * @inheritdoc
      */
@@ -46,11 +33,10 @@ class Ieo extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['persona_id', 'institucion_id', 'sede_id', 'proyecto_id', 'estado'], 'default', 'value' => null],
-            [['persona_id', 'institucion_id', 'sede_id', 'proyecto_id', 'estado'], 'integer'],
+            [['institucion_id', 'sede_id', 'estado'], 'default', 'value' => null],
+            [['institucion_id', 'sede_id', 'estado'], 'integer'],
+            [['persona_acargo', 'codigo_dane', 'zona_educativa', 'comuna', 'barrio'], 'string'],
             [['institucion_id'], 'exist', 'skipOnError' => true, 'targetClass' => Instituciones::className(), 'targetAttribute' => ['institucion_id' => 'id']],
-            [['persona_id'], 'exist', 'skipOnError' => true, 'targetClass' => Personas::className(), 'targetAttribute' => ['persona_id' => 'id']],
-            [['proyecto_id'], 'exist', 'skipOnError' => true, 'targetClass' => ProyectoIeo::className(), 'targetAttribute' => ['proyecto_id' => 'id']],
             [['sede_id'], 'exist', 'skipOnError' => true, 'targetClass' => Sedes::className(), 'targetAttribute' => ['sede_id' => 'id']],
         ];
     }
@@ -62,74 +48,14 @@ class Ieo extends \yii\db\ActiveRecord
     {
         return [
             'id' => 'ID',
-            'persona_id' => 'Profesional encargado',
-            'institucion_id' => 'Institución',
-            'sede_id' => 'Sede',
-            'proyecto_id' => 'Proyecto',
+            'institucion_id' => 'Institucion ID',
+            'sede_id' => 'Sede ID',
+            'estado' => 'Estado',
+            'persona_acargo' => 'Persona Acargo',
+            'codigo_dane' => 'Codigo Dane',
+            'zona_educativa' => 'Zona Educativa',
+            'comuna' => 'Comuna',
+            'barrio' => 'Barrio',
         ];
-    }
-
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getDocumentosReconocimientos()
-    {
-        return $this->hasMany(DocumentosReconocimiento::className(), ['ieo_id' => 'id']);
-    }
-
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getEvidencias()
-    {
-        return $this->hasMany(Evidencias::className(), ['ieo_id' => 'id']);
-    }
-
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getInstitucion()
-    {
-        return $this->hasOne(Instituciones::className(), ['id' => 'institucion_id']);
-    }
-
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getPersona()
-    {
-        return $this->hasOne(Personas::className(), ['id' => 'persona_id']);
-    }
-
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getProyecto()
-    {
-        return $this->hasOne(ProyectoIeo::className(), ['id' => 'proyecto_id']);
-    }
-
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getSede()
-    {
-        return $this->hasOne(Sedes::className(), ['id' => 'sede_id']);
-    }
-
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getProductos()
-    {
-        return $this->hasMany(Producto::className(), ['ieo_id' => 'id']);
-    }
-
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getTiposCantidadPoblacions()
-    {
-        return $this->hasMany(TiposCantidadPoblacion::className(), ['ieo_id' => 'id']);
     }
 }
