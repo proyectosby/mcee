fc40d1421a325e8b5982ce0b6cb23919175aeafd
Oscar-DLopez <36933192+Oscar-DLopez@users.noreply.github.com>
2018-10-23 20:21:44 -0500

    cambios en el formulario informe planeacion ieo

--- web/js/ecInformePlaneacionIeo.js
+++ web/js/ecInformePlaneacionIeo.js
@@ -1,20 +1,73 @@
 $( document ).ready(function(){
 	
-	
-swal({
-  title: '<strong>HTML <u>example</u></strong>',
-  type: 'info',
-  html:
-    'Cuál es el estado general de avance de los procesos de gestión de los Proyectos Pedagógicos Transversales <br>'+
-	'<progress max="100" value="80" ></progress> 80%',
-	
-  focusConfirm: false,
-  confirmButtonText:
-    '<i class="fa fa-thumbs-up"></i> Gracias!',
-  confirmButtonAriaLabel: 'Thumbs up, great!',
-  cancelButtonText:
-    '<i class="fa fa-thumbs-down"></i>',
-  cancelButtonAriaLabel: 'Thumbs down',
-})
+
 	
 });
+
+//semaforizacion segun documento 3A
+function colorBarra(valor)
+{
+	color ="";
+	if(valor < 50)
+	{
+		color = "red";
+	}
+	else if(valor < 70)
+	{
+		color = "yellow";
+	}
+	else if (valor < 100)
+	{
+		color = "green";
+	}
+	else
+	{
+		color ="blue";
+	}
+	return color;
+}
+
+//calcula los porcentaje de avances 
+$( "#porcentajes" ).click(function() 
+{
+	estadoActual2 = $("#ecavances-2-estado_actual").val()*1;
+	estadoActual1 = $("#ecavances-1-estado_actual").val()*1;
+	estadoActual3 = $("#ecavances-3-estado_actual").val()*1;
+	
+	PorcentajeAvanceProceso = Math.trunc(((estadoActual1 + estadoActual2 + estadoActual3)/12)*100);
+	
+	$("#porcentajeAvance1").css({"width": ""+PorcentajeAvanceProceso+"%","background-color":""+colorBarra(PorcentajeAvanceProceso)}).text(PorcentajeAvanceProceso+"%");
+
+	
+	estadoActual4 = $("#ecavances-4-estado_actual").val()*1;
+	estadoActual6 = $("#ecavances-6-estado_actual").val()*1;
+	estadoActual5 = $("#ecavances-5-estado_actual").val()*1;
+
+	porcentajeAvanceEstrategias = Math.trunc(((estadoActual4 + estadoActual6 + estadoActual5)/12)*100);
+	
+	
+	$("#porcentajeAvance2").css({"width": ""+porcentajeAvanceEstrategias+"%","background-color":""+colorBarra(porcentajeAvanceEstrategias)}).text(porcentajeAvanceEstrategias+"%");
+	
+	estadoActual9 = $("#ecavances-9-estado_actual").val()*1;
+	estadoActual8 = $("#ecavances-8-estado_actual").val()*1;
+	estadoActual7 = $("#ecavances-7-estado_actual").val()*1;
+	
+	porcentajeAvanceOrientaciones = Math.trunc(((estadoActual9 + estadoActual8 + estadoActual7)/12)*100);
+	
+	
+	$("#porcentajeAvance3").css({"width": ""+porcentajeAvanceOrientaciones+"%","background-color":""+colorBarra(porcentajeAvanceOrientaciones)}).text(porcentajeAvanceOrientaciones+"%");
+	
+	estadoActual9 = $("#ecavances-9-estado_actual").val()*1;
+	estadoActual8 = $("#ecavances-8-estado_actual").val()*1;
+	estadoActual7 = $("#ecavances-7-estado_actual").val()*1;
+	
+	porcentajeAvanceProductos = Math.trunc(((estadoActual9 + estadoActual8 + estadoActual7)/12)*100);
+	
+	$("#porcentajeAvance3").css({"width": ""+porcentajeAvanceProductos+"%","background-color":""+colorBarra(porcentajeAvanceProductos)}).text(porcentajeAvanceProductos+"%");
+	
+	
+	
+	
+	
+});
+
