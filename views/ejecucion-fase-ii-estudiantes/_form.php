243a5ed8f898b284ff771787b9ffc70c62c64202
Edwin Molina <edwin.molina@byallgraphic.com>
2018-11-08 21:28:59 -0500

    Se hacen cambios varios
    Se calcula los campos de condiciones institucionales según lo que ingrese el usuario en la ejecución de fase

--- views/ejecucion-fase-ii-estudiantes/_form.php
+++ views/ejecucion-fase-ii-estudiantes/_form.php
@@ -189,19 +189,43 @@ if( $guardado ){
 				</div>
 				
 				<div class='col-sm-1'>
-					<?= $form->field($condiciones, "participantes_por_curso")->textarea([ 'class' => 'form-control', 'maxlength' => true, 'data-type' => 'number'])->label( null, [ 'style' => 'display:none' ]) ?>
+					<?= $form->field($condiciones, "participantes_por_curso")
+							->textarea([ 
+								'class' 		=> 'form-control', 
+								'maxlength' 	=> true, 
+								'data-type' 	=> 'number',
+								'data-disabled' => 'true',
+							])->label( null, [ 'style' => 'display:none' ]) ?>
 				</div>
 				
 				<div class='col-sm-1'>
-					<?= $form->field($condiciones, "promedio_estudiantes_por_curso")->textarea([ 'class' => 'form-control', 'maxlength' => true, 'data-type' => 'number'])->label( null, [ 'style' => 'display:none' ]) ?>
+					<?= $form->field($condiciones, "promedio_estudiantes_por_curso")
+							->textarea([ 
+								'class' 		=> 'form-control', 
+								'maxlength' 	=> true, 
+								'data-type' 	=> 'number',
+								'data-disabled' => 'true',
+							])->label( null, [ 'style' => 'display:none' ]) ?>
 				</div>
 				
 				<div class='col-sm-1'>
-					<?= $form->field($condiciones, "total_sesiones")->textarea([ 'class' => 'form-control', 'maxlength' => true, 'data-type' => 'number'])->label( null, [ 'style' => 'display:none' ]) ?>
+					<?= $form->field($condiciones, "total_sesiones")
+							->textarea([ 
+								'class' 		=> 'form-control', 
+								'maxlength' 	=> true, 
+								'data-type' 	=> 'number',
+								'data-disabled' => 'true',
+							])->label( null, [ 'style' => 'display:none' ]) ?>
 				</div>
 				
 				<div class='col-sm-1'>
-					<?= $form->field($condiciones, "total_estudiantes")->textarea([ 'class' => 'form-control', 'maxlength' => true, 'data-type' => 'text'])->label( null, [ 'style' => 'display:none' ]) ?>
+					<?= $form->field($condiciones, "total_estudiantes")
+							->textarea([ 
+								'class' 		=> 'form-control', 
+								'maxlength' 	=> true, 
+								'data-type' 	=> 'text',
+								'data-disabled' => 'true',
+							])->label( null, [ 'style' => 'display:none' ]) ?>
 				</div>
 
 			</div>
