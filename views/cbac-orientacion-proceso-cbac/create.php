c1148a300673d5b37270829c6260f1de6bba371b
aarias <aarias409@gmail.com>
2018-12-23 11:07:44 -0500

    Registro formulario cbac-orientacion-proceso-cbac

---
+++ views/cbac-orientacion-proceso-cbac/create.php
@@ -0,0 +1,23 @@
+<?php
+
+use yii\helpers\Html;
+
+
+/* @var $this yii\web\View */
+/* @var $model app\models\CbacOrientacionProcesoCbac */
+
+$this->title = 'Nuevo Orientación del proceso Competencias Basicas Arte y Cultura';
+$this->params['breadcrumbs'][] = ['label' => 'Cbac Orientacion Proceso Cbacs', 'url' => ['index']];
+$this->params['breadcrumbs'][] = "Agregar";
+?>
+<div class="cbac-orientacion-proceso-cbac-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+        'sedes' => $sedes,
+        'institucion' => $institucion,
+    ]) ?>
+
+</div>
