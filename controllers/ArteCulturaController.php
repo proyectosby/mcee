3ec3d023b58068e4f7f541ed723c89f92ecd14f0
vivianaRodas <36738664+vivianaRodas@users.noreply.github.com>
2018-12-27 22:52:55 -0500

    Se crea index de arte y cultura

---
+++ controllers/ArteCulturaController.php
@@ -0,0 +1,90 @@
+<?php
+/**********
+---------------------------------------
+Versión: 001
+Fecha: 27-12-2018
+Desarrollador: Maria Viviana Rodas
+Descripción: Controlador de la vista que contiene los botones de arte y cultura
+---------------------------------------
+
+**********/
+namespace app\controllers;
+
+if(@$_SESSION['sesion']=="si")
+{ 
+	// echo $_SESSION['nombre'];
+} 
+//si no tiene sesion se redirecciona al login
+else
+{
+	header('Location: index.php?r=site%2Flogin');
+	die;
+}
+
+use Yii;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use yii\helpers\ArrayHelper;
+use app\models\Estudiantes;
+use yii\data\SqlDataProvider;
+
+
+use app\models\Asignaturas;
+use app\models\AsginaturasBuscar;
+use app\models\Estados;
+use app\models\Sedes;
+use app\models\Instituciones;
+use app\models\Paralelos;
+use app\models\SedesJornadas;
+use app\models\SemillerosDatosIeo;
+use app\models\Personas;
+use yii\data\ActiveDataProvider;
+use yii\data\ArrayDataProvider;
+
+
+
+
+/**
+ * AsignaturasController implements the CRUD actions for Asignaturas model.
+ */
+class ArteCulturaController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+	
+	
+	
+	
+
+
+    /**
+     * Lists all Asignaturas models.
+     * @return mixed
+     */
+	 //recibe 2 parametros con la intencion de filtrar por institucion y por sede
+    // public function actionIndex($idInstitucion = 0, $idSedes = 0)
+    public function actionIndex()
+    {
+		
+	
+			return $this->render('index', [
+				
+			]);
+		
+    }
+
+    
+}
