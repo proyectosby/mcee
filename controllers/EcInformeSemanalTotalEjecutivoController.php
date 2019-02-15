bb12221f2b261fec62144bb531983c3a7e7c8e9d
Oscar-DLopez <36933192+Oscar-DLopez@users.noreply.github.com>
2019-02-15 17:12:50 -0500

    avances reporte total ejecutivo 5 y 6

--- controllers/EcInformeSemanalTotalEjecutivoController.php
+++ controllers/EcInformeSemanalTotalEjecutivoController.php
@@ -16,6 +16,7 @@ else
 use Yii;
 use app\models\EcInformeSemanalTotalEjecutivo;
 use app\models\Sedes;
+use app\models\Instituciones;
 use yii\data\ActiveDataProvider;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
@@ -120,13 +121,24 @@ class EcInformeSemanalTotalEjecutivoController extends Controller
 		$sedes = Sedes::find()->andWhere(" estado =1 ")->orderby( 'id' )->all();
 		$sedes = ArrayHelper::map( $sedes, 'id', 'id_instituciones' );
 			
+		
+		$instituciones = Instituciones::find()->andWhere(" estado =1 ")->orderby( 'id' )->all();
+		$instituciones = ArrayHelper::map( $instituciones, 'id', 'descripcion' );		
+			
+			
         $connection = Yii::$app->getDb();
 		$command = $connection->createCommand("
 		select 
 			ise.institucion_id, 
-			ise.sede_id, ise.id_tipo_informe,
+			ise.sede_id,
+			ise.id_tipo_informe,
 			ai.avance_sede,
-			ai.avance_ieo
+			ai.avance_ieo,
+			ai.actividad_1_porcentaje,
+			ai.actividad_2_porcentaje,
+			ai.actividad_3_porcentaje,
+			ise.fecha_inicio,
+			ise.fecha_fin
 		from 
 			ec.informe_semanal_ejecucion_ise as ise,
 			ec.actividades_ise as ai
@@ -137,95 +149,174 @@ class EcInformeSemanalTotalEjecutivoController extends Controller
 		");
 		$result = $command->queryAll();
 		
-		// echo "<pre>"; print_r($result); echo "</pre>"; 
-		// die;
-		
-		foreach ($result as $r)
-		{
-			$datos[$r['institucion_id']]["i" . $r['id_tipo_informe']]['avance_ieo'] = $r['avance_ieo'];
-			$datos[$r['institucion_id']][$r['sede_id']][$r['id_tipo_informe']] = $r['avance_sede'];
-			
-		}
-		
-		$cantidaEjecutadaIEO  = 0;
-		$cantidaEjecutadaSedePPT  = 0;
-		$cantidaEjecutadaSedePSSE = 0;
-		$cantidaEjecutadaSedePAF  = 0;
-		$cantidaTotalSedesXIEOPPT = 0;
-		$cantidaTotalSedesXIEOPSSE = 0;
-		$cantidaTotalSedesXIEOPAF = 0;
-		$datosSedesxIEO = array();
-		$arrayTipoInforme= array(7,19,31);
 	
-		foreach ($sedes as $idSede => $idInstitucion)
+		// echo "<pre>"; print_r($result); echo "</pre>"; 
+		foreach ($result as $r)
+		{	
+			$datos[$r['fecha_inicio']. " A " .$r['fecha_fin']][$r['institucion_id']][$r['sede_id']][$r['id_tipo_informe']]['avance_sede'] = $r['avance_sede'];
+			$datos[$r['fecha_inicio']. " A " .$r['fecha_fin']][$r['institucion_id']][$r['sede_id']][$r['id_tipo_informe']]['actividad_1_porcentaje'] = $r['actividad_1_porcentaje'];
+			$datos[$r['fecha_inicio']. " A " .$r['fecha_fin']][$r['institucion_id']][$r['sede_id']][$r['id_tipo_informe']]['actividad_2_porcentaje'] = $r['actividad_2_porcentaje'];
+			$datos[$r['fecha_inicio']. " A " .$r['fecha_fin']][$r['institucion_id']][$r['sede_id']][$r['id_tipo_informe']]['actividad_3_porcentaje'] = $r['actividad_3_porcentaje'];	
+		}
+		
+		// echo "<pre>"; print_r($datos); echo "</pre>"; 
+		
+		$avanceSede = [];
+		$avanceIEO = [];
+		$activiadad1 = [];
+		foreach($datos as $key => $valor)
 		{
-			
-			$datosSedesxIEO[$idInstitucion][$idSede][7] = @$datos[$idInstitucion][$idSede][7] ;
-			$datosSedesxIEO[$idInstitucion][$idSede][19] = @$datos[$idInstitucion][$idSede][19] ;
-			$datosSedesxIEO[$idInstitucion][$idSede][31] = @$datos[$idInstitucion][$idSede][31] ;
-			
-			
-			
-			
-			// $cantidaTotalSedesXIEO += @$datos[$idInstitucion][$idSede][$tipo];
-			
-			//ppt
-			if (@$datos[$idInstitucion][$idSede][7] == 100)
+			foreach($valor as $ieo)
 			{
-				$cantidaEjecutadaSedePPT++;
+				foreach($ieo as $sede)
+				{
+					//Saber cuantas sedes estan al 100% en el rango de fecha (semana)
+					//PPT 7
+					if(@$sede[7]['avance_sede'] == 100)
+					{
+						@$avanceSede[$key][7]+= 1;
+					}
+					
+					//PSSE	19
+					if(@$sede[19]['avance_sede'] == 100)
+					{
+						@$avanceSede[$key][19]+= 1;
+					}
+					
+					//PAF 31
+					if(@$sede[31]['avance_sede'] == 100)
+					{
+						@$avanceSede[$key][31]+= 1;
+					}
+					
+					@$activiadades[$key][7]['actividad_1_porcentaje'] += $sede[7]['actividad_1_porcentaje'];
+					@$activiadades[$key][7]['actividad_2_porcentaje'] += $sede[7]['actividad_2_porcentaje'];
+					@$activiadades[$key][7]['actividad_3_porcentaje'] += $sede[7]['actividad_3_porcentaje'];
+					@$activiadades[$key][19]['actividad_1_porcentaje'] += $sede[19]['actividad_1_porcentaje'];
+					@$activiadades[$key][19]['actividad_2_porcentaje'] += $sede[19]['actividad_2_porcentaje'];
+					@$activiadades[$key][19]['actividad_3_porcentaje'] += $sede[19]['actividad_3_porcentaje'];
+					@$activiadades[$key][31]['actividad_1_porcentaje'] += $sede[31]['actividad_1_porcentaje'];
+					@$activiadades[$key][31]['actividad_2_porcentaje'] += $sede[31]['actividad_2_porcentaje'];
+					@$activiadades[$key][31]['actividad_3_porcentaje'] += $sede[31]['actividad_3_porcentaje'];
+					
+				}
 				
+					//Saber cuantas IEO estan al 100% en el rango de fecha (semana)
+				//PPT 7
+				if(@$avanceSede[$key][7] / count($ieo) == 1)
+				{
+					@$avanceIEO[$key][7]+=1;
+				}
+					
+				//PSSE	19
+				if(@$avanceSede[$key][19] / count($ieo) == 1)
+				{
+					@$avanceIEO[$key][19]+=1;
+				}
+				
+				//PAF 31
+				if(@$avanceSede[$key][31] / count($ieo) == 1)
+				{
+					@$avanceIEO[$key][31]+=1;
+				}
 			}
+		}
+		
+	
+		
+		$porcentajeIEO = [];
+		$cantidadIEO = count($instituciones);
+
+		foreach($avanceIEO as $key => $aIEO) 
+		{
+			@$porcentajeIEO[$key][7] = round ( $aIEO[7] / 45  * 100) . "%";
+			@$porcentajeIEO[$key][19] = round ( $aIEO[19] / 45  * 100) . "%";
+			@$porcentajeIEO[$key][31] = round ( $aIEO[31] / 45 * 100) . "%";
+		}
+		// echo "<pre>"; print_r($porcentajeIEO); echo "</pre>"; 		
+		
+		// echo "<pre>"; print_r($avanceIEO); echo "</pre>"; 
+		
+		//porcentaje sobre la actividad 1 2 y 3
+		$cantidadSedes = count($sedes);
+		
+		foreach($activiadades as $key => $actividad )
+		{
+			@$activiadades[$key][7]['actividad_1_porcentaje'] = round ( $actividad[7]['actividad_1_porcentaje'] / $cantidadSedes * 100 ) . "%";
+			@$activiadades[$key][7]['actividad_2_porcentaje'] = round ( $actividad[7]['actividad_2_porcentaje'] / $cantidadSedes * 100 ) . "%";
+			@$activiadades[$key][7]['actividad_3_porcentaje'] = round ( $actividad[7]['actividad_3_porcentaje'] / $cantidadSedes * 100 ) . "%";
+			@$activiadades[$key][19]['actividad_1_porcentaje'] = round ( $actividad[19]['actividad_1_porcentaje'] / $cantidadSedes * 100 ) . "%";
+			@$activiadades[$key][19]['actividad_2_porcentaje'] = round ( $actividad[19]['actividad_2_porcentaje'] / $cantidadSedes * 100 ) . "%";
+			@$activiadades[$key][19]['actividad_3_porcentaje'] = round ( $actividad[19]['actividad_3_porcentaje'] / $cantidadSedes * 100 ) . "%";
+			@$activiadades[$key][31]['actividad_1_porcentaje'] = round ( $actividad[31]['actividad_1_porcentaje'] / $cantidadSedes * 100 ) . "%";
+			@$activiadades[$key][31]['actividad_2_porcentaje'] = round ( $actividad[31]['actividad_2_porcentaje'] / $cantidadSedes * 100 ) . "%";
+			@$activiadades[$key][31]['actividad_3_porcentaje'] = round ( $actividad[31]['actividad_3_porcentaje'] / $cantidadSedes * 100 ) . "%";
+		}
+	
+		$porcentajesSedes = [];
+		foreach ($avanceSede as $key => $aSedes )
+		{
+			@$porcentajesSedes[$key][7] = round ( $aSedes[7] / $cantidadSedes  * 100) . "%";
+			@$porcentajesSedes[$key][19] = round ( $aSedes[19] / 59  * 100) . "%";
+			@$porcentajesSedes[$key][31] = round ( $aSedes[31] / $cantidadSedes * 100) . "%";
+		}
+		
 			
-			//PSSE
-			if (@$datos[$idInstitucion][$idSede][19] == 100)
-			{
-				$cantidaEjecutadaSedePSSE++;
-			}
+		$connection = Yii::$app->getDb();
+		$command = $connection->createCommand("
+		SELECT 
+			ise.fecha_inicio,
+			ise.fecha_fin,
+			ise.id_tipo_informe,
+			tcpi.tiempo_libre, 
+			tcpi.edu_derechos, 
+			tcpi.sexualidad_ciudadania, 
+			tcpi.medio_ambiente
+		FROM 
+			ec.tipo_cantidad_poblacion_ise as tcpi, 
+			ec.informe_semanal_ejecucion_ise as ise
+		WHERE 
+			tcpi.id_informe_semanal_ejecucion_ise  = ise.id
+		");
+		$result1 = $command->queryAll();
+		
+		
 			
-			//PAF
-			if (@$datos[$idInstitucion][$idSede][31] == 100)
-			{
-				$cantidaEjecutadaSedePAF++;
-			}
+		
+		echo "<pre>"; print_r($result1); echo "</pre>"; 
+		
+		$poblacionBenficiada =  [];
+		foreach($result1 as $d )
+		{
+			@$poblacionBenficiada[$d['fecha_inicio'] . " A " .$d['fecha_fin']][$d['id_tipo_informe']] += $d['tiempo_libre'];
+            @$poblacionBenficiada[$d['fecha_inicio'] . " A " .$d['fecha_fin']][$d['id_tipo_informe']] += $d['edu_derechos'];
+            @$poblacionBenficiada[$d['fecha_inicio'] . " A " .$d['fecha_fin']][$d['id_tipo_informe']] += $d['sexualidad_ciudadania'];
+            @$poblacionBenficiada[$d['fecha_inicio'] . " A " .$d['fecha_fin']][$d['id_tipo_informe']] += $d['medio_ambiente'];
 			
 		}
 		
-		foreach ($datos as $dato )
-		{
-			
-			//ppt
-			if( @$dato['i7']['avance_ieo'] == 100)
-			{
-			
-				$cantidaTotalSedesXIEOPPT++;
-			}
-			
-			// PSSE
-			if( @$dato['i19']['avance_ieo'] == 100)
-			{
-				$cantidaTotalSedesXIEOPSSE++;
-			}
-			
-			// PAF
-			if( @$dato['i31']['avance_ieo'] == 100)
-			{
-				$cantidaTotalSedesXIEOPAF++;
-			}
-		}
+		echo "<pre>"; print_r($poblacionBenficiada); echo "</pre>"; 
+		
+		//porcentaje sobre Sedes
+		die;
+	
+	
 		
         $model = new EcInformeSemanalTotalEjecutivo();
     
         return $this->render('create', [
             'model' => $model,
             'guardado' => 0,
-            'cantidaEjecutadaSedePPT' => $cantidaEjecutadaSedePPT,
-            'cantidaEjecutadaSedePSSE' => $cantidaEjecutadaSedePSSE,
-			'cantidaEjecutadaSedePAF' => $cantidaEjecutadaSedePAF,
-			'cantidaTotalSedesXIEOPPT'  => $cantidaTotalSedesXIEOPPT,
-			'cantidaTotalSedesXIEOPSSE' => $cantidaTotalSedesXIEOPSSE,
-			'cantidaTotalSedesXIEOPAF'  => $cantidaTotalSedesXIEOPAF,
+       
+			
         ]);
     }
+	
+	public function porcentaje($valor , $porcentaje)
+	{
+		return  explode(".",$valor / $porcentaje * 100 )[0] . "%" ;
+	}
+
 
     /**
      * Updates an existing EcInformeSemanalTotalEjecutivo model.
--- views/ec-informe-semanal-total-ejecutivo/_form.php
+++ views/ec-informe-semanal-total-ejecutivo/_form.php
@@ -3,11 +3,14 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
+use fedemotta\datatables\DataTables;
+use app\models\CbacInformeSemanalCac;
+use yii\data\ActiveDataProvider;
 /* @var $this yii\web\View */
 /* @var $model app\models\EcInformeSemanalTotalEjecutivo */
 /* @var $form yii\widgets\ActiveForm */
-//$this->registerJsFile("https://unpkg.com/sweetalert/dist/sweetalert.min.js");
-$this->registerJsFile(Yii::$app->request->baseUrl.'/js/documentos.js',['depends' => [\yii\web\JqueryAsset::className()]]);
+
+
 
 if( isset($_GET['guardado']) && $_GET['guardado'] == 1 ){    
     echo Html::hiddenInput( 'guardado', '1' );
@@ -19,18 +22,18 @@ $this->registerJsFile(
 );
 
 
-$this->registerJsFile(
-	"@web/js/ecresumensemanalEjecutivo.js", 
-	[
-		'depends' => [
-						\yii\web\JqueryAsset::className(),
-						\fedemotta\datatables\DataTablesAsset::className(),
-						\fedemotta\datatables\DataTablesTableToolsAsset::className(),
-					],
-	]
-);
+$this->registerJsFile(Yii::$app->request->baseUrl.'/js/informe-semanal-total-ejecutivo.js',['depends' => [\yii\web\JqueryAsset::className()]]);
+
+
+	 $dataProvider = new ActiveDataProvider([
+            'query' => CbacInformeSemanalCac::find(),
+        ]);
 
 ?>
+
+  <!--<?= DataTables::widget(['dataProvider' => $dataProvider,     ]); ?> -->
+
+
 <style>
     
     table {
@@ -61,7 +64,12 @@ $this->registerJsFile(
 		border: 1px solid #ddd;
 	}
 
+tr.group, tr.group:hover 
+{
+    background-color: #ddd !important;
+}
 </style>
+
 
 <div class="ec-informe-semanal-total-ejecutivo-form">
 
@@ -93,66 +101,7 @@ $this->registerJsFile(
                 <th rowspan='1' colspan='1'>Alarmas generales</th>
             </tr>
         </thead>
-        <tbody>
-
-            <tr>
-                <td rowspan='1' colspan='1'>PPT</td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[0]cantidad_ieo')->textInput([ 'type' => 'number', 'value' => $cantidaTotalSedesXIEOPPT ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[0]cantidad_sedes')->textInput([ 'type' => 'number', 'value' =>  $cantidaEjecutadaSedePPT ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[0]porcentaje_ieo')->textInput([ 'type' => 'number', 'value' => isset($porcentaje_ieo[1]) ? $porcentaje_ieo[1] : '' ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[0]porcentaje_sedes')->textInput([ 'type' => 'number', 'value' => isset($porcentaje_sedes[1]) ? $porcentaje_sedes[1] : '' ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[0]porcentaje_actividad_uno')->textInput([ 'type' => 'number', 'value' => isset($porcentaje_actividad_uno[1]) ? $porcentaje_actividad_uno[1] : '']) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[0]porcentaje_actividad_dos')->textInput([ 'type' => 'number', 'value' => isset($porcentaje_actividad_dos[1]) ? $porcentaje_actividad_dos[1] : '' ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[0]porcentaje_actividad_tres')->textInput([ 'type' => 'number', 'value' => isset($porcentaje_actividad_tres[1]) ? $porcentaje_actividad_tres[1] : '' ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[0]poblacion_beneficiada_directa')->textInput([ 'type' => 'number', 'value' => isset($poblacion_beneficiada_directa[1]) ? $poblacion_beneficiada_directa[1] : '' ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[0]poblacion_beneficiada_indirecta')->textInput([ 'type' => 'number', 'value' => isset($poblacion_beneficiada_indirecta[1]) ? $poblacion_beneficiada_indirecta[1] : '' ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[0]alarmas_generales')->textInput([ 'type' => 'number', 'value' => isset($alarmas_generales[1]) ? $alarmas_generales[1] : '']) ?></td>                
-            </tr>
-
-            <tr>
-                <td rowspan='1' colspan='1'>PSSE</td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[1]cantidad_ieo')->textInput([ 'type' => 'number', 'value' =>  $cantidaTotalSedesXIEOPSSE ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[1]cantidad_sedes')->textInput([ 'type' => 'number', 'value' => $cantidaEjecutadaSedePSSE ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[1]porcentaje_ieo')->textInput([ 'type' => 'number', 'value' => isset($porcentaje_ieo[2]) ? $porcentaje_ieo[2] : '' ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[1]porcentaje_sedes')->textInput([ 'type' => 'number', 'value' => isset($porcentaje_sedes[2]) ? $porcentaje_sedes[2] : '' ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[1]porcentaje_actividad_uno')->textInput([ 'type' => 'number', 'value' => isset($porcentaje_actividad_uno[2]) ? $porcentaje_actividad_uno[2] : '']) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[1]porcentaje_actividad_dos')->textInput([ 'type' => 'number', 'value' => isset($porcentaje_actividad_dos[2]) ? $porcentaje_actividad_dos[2] : '' ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[1]porcentaje_actividad_tres')->textInput([ 'type' => 'number', 'value' => isset($porcentaje_actividad_tres[2]) ? $porcentaje_actividad_tres[2] : '' ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[1]poblacion_beneficiada_directa')->textInput([ 'type' => 'number', 'value' => isset($poblacion_beneficiada_directa[2]) ? $poblacion_beneficiada_directa[2] : '' ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[1]poblacion_beneficiada_indirecta')->textInput([ 'type' => 'number', 'value' => isset($poblacion_beneficiada_indirecta[2]) ? $poblacion_beneficiada_indirecta[2] : '' ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[1]alarmas_generales')->textInput([ 'type' => 'number', 'value' => isset($alarmas_generales[2]) ? $alarmas_generales[2] : '']) ?></td>               
-            </tr>
-
-            <tr>
-                <td rowspan='1' colspan='1'>PAF</td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[2]cantidad_ieo')->textInput([ 'type' => 'number', 'value' => $cantidaTotalSedesXIEOPAF ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[2]cantidad_sedes')->textInput([ 'type' => 'number', 'value' => $cantidaEjecutadaSedePAF ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[2]porcentaje_ieo')->textInput([ 'type' => 'number', 'value' => isset($porcentaje_ieo[3]) ? $porcentaje_ieo[3] : '' ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[2]porcentaje_sedes')->textInput([ 'type' => 'number', 'value' => isset($porcentaje_sedes[3]) ? $porcentaje_sedes[3] : '' ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[2]porcentaje_actividad_uno')->textInput([ 'type' => 'number', 'value' => isset($porcentaje_actividad_uno[3]) ? $porcentaje_actividad_uno[3] : '']) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[2]porcentaje_actividad_dos')->textInput([ 'type' => 'number', 'value' => isset($porcentaje_actividad_dos[3]) ? $porcentaje_actividad_dos[3] : '' ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[2]porcentaje_actividad_tres')->textInput([ 'type' => 'number', 'value' => isset($porcentaje_actividad_tres[3]) ? $porcentaje_actividad_tres[3] : '' ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[2]poblacion_beneficiada_directa')->textInput([ 'type' => 'number', 'value' => isset($poblacion_beneficiada_directa[3]) ? $poblacion_beneficiada_directa[3] : '' ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[2]poblacion_beneficiada_indirecta')->textInput([ 'type' => 'number', 'value' => isset($poblacion_beneficiada_indirecta[3]) ? $poblacion_beneficiada_indirecta[3] : '' ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[2]alarmas_generales')->textInput([ 'type' => 'number', 'value' => isset($alarmas_generales[3]) ? $alarmas_generales[3] : '']) ?></td>                
-            </tr>
-
-           
-            <tr>
-                <td rowspan='1' colspan='1'>TOTAL</td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[4]cantidad_ieo')->textInput([ 'type' => 'number', 'value' => isset($cantidad_ieo[1]) ? $cantidad_ieo[1] + $cantidad_ieo[2] +$cantidad_ieo[3] : '', 'readonly' => true]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[4]cantidad_sedes')->textInput([ 'type' => 'number', 'value' => isset($cantidad_sedes_ieo[1]) ? $cantidad_sedes_ieo[1] + $cantidad_sedes_ieo[2]+ $cantidad_sedes_ieo[3] : '', 'readonly' => true]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[4]porcentaje_ieo')->textInput([ 'type' => 'number', 'value' => isset($porcentaje_ieo[3]) ? ((($porcentaje_ieo[3]+ $porcentaje_ieo[2] + $porcentaje_ieo[1]) / 3) .'%') : '', 'readonly' => true ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[4]porcentaje_sedes')->textInput([ 'type' => 'number', 'value' => isset($porcentaje_sedes[3]) ? ((($porcentaje_sedes[3]+ $porcentaje_sedes[2] + $porcentaje_sedes[1]) / 3) .'%') : '', 'readonly' => true ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[4]porcentaje_actividad_uno')->textInput([ 'type' => 'number', 'value' => isset($porcentaje_actividad_uno[3]) ? ((($porcentaje_actividad_uno[3]+ $porcentaje_actividad_uno[2] + $porcentaje_actividad_uno[1]) / 3) .'%') : '', 'readonly' => true]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[4]porcentaje_actividad_dos')->textInput([ 'type' => 'number', 'value' =>  isset($porcentaje_actividad_dos[3]) ? ((($porcentaje_actividad_dos[3]+ $porcentaje_actividad_dos[2] + $porcentaje_actividad_dos[1]) / 3) .'%') : '', 'readonly' => true]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[4]porcentaje_actividad_tres')->textInput([ 'type' => 'number', 'value' =>  isset($porcentaje_actividad_tres[3]) ? ((($porcentaje_actividad_tres[3]+ $porcentaje_actividad_tres[2] + $porcentaje_actividad_tres[1]) / 3) .'%') : '', 'readonly' => true]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[4]poblacion_beneficiada_directa')->textInput([ 'type' => 'number', 'value' => isset($poblacion_beneficiada_directa[1]) ? ($poblacion_beneficiada_directa[1] + $poblacion_beneficiada_directa[2] +$poblacion_beneficiada_directa[3]) : '', 'readonly' => true ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[4]poblacion_beneficiada_indirecta')->textInput([ 'type' => 'number', 'value' => isset($poblacion_beneficiada_indirecta[1]) ? ($poblacion_beneficiada_indirecta[1] + $poblacion_beneficiada_indirecta[2] +$poblacion_beneficiada_indirecta[3]) : '', 'readonly' => true ]) ?></td>
-                <td rowspan='1' colspan='1'><?= $form->field($model, '[4]alarmas_generales')->textInput([ 'type' => 'number', 'value' => isset($alarmas_generales[1], $alarmas_generales[2],$alarmas_generales[3]) ? (intval($alarmas_generales[1]) + intval($alarmas_generales[2]) +intval($alarmas_generales[3])) : '', 'readonly' => true]) ?></td>                
-            </tr>
-        </tbody>
-
+      
 
     </table>
 
@@ -161,5 +110,182 @@ $this->registerJsFile(
     </div>
 
     <?php ActiveForm::end(); ?>
-
+    
+	
+	
 </div>
+
+
+<table id="example" class="display" style="width:100%">
+        <thead>
+            <tr>
+                <th>First name</th>
+                <th>Last name</th>
+                <th>Position</th>
+                <th>Office</th>
+                <th>Salary</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Tiger</td>
+                <td>Nixon</td>
+                <td>System Architect</td>
+                <td>Edinburgh</td>
+                <td>$320,800</td>
+            </tr>
+            <tr>
+                <td>Garrett</td>
+                <td>Winters</td>
+                <td>Accountant</td>
+                <td>Tokyo</td>
+                <td>$170,750</td>
+            </tr>
+            <tr>
+                <td>Ashton</td>
+                <td>Cox</td>
+                <td>Junior Technical Author</td>
+                <td>San Francisco</td>
+                <td>$86,000</td>
+            </tr>
+            <tr>
+                <td>Cedric</td>
+                <td>Kelly</td>
+                <td>Senior Javascript Developer</td>
+                <td>Edinburgh</td>
+                <td>$433,060</td>
+            </tr>
+            <tr>
+                <td>Airi</td>
+                <td>Satou</td>
+                <td>Accountant</td>
+                <td>Tokyo</td>
+                <td>$162,700</td>
+            </tr>
+            <tr>
+                <td>Brielle</td>
+                <td>Williamson</td>
+                <td>Integration Specialist</td>
+                <td>New York</td>
+                <td>$372,000</td>
+            </tr>
+            <tr>
+                <td>Herrod</td>
+                <td>Chandler</td>
+                <td>Sales Assistant</td>
+                <td>San Francisco</td>
+                <td>$137,500</td>
+            </tr>
+            <tr>
+                <td>Rhona</td>
+                <td>Davidson</td>
+                <td>Integration Specialist</td>
+                <td>Tokyo</td>
+                <td>$327,900</td>
+            </tr>
+            <tr>
+                <td>Colleen</td>
+                <td>Hurst</td>
+                <td>Javascript Developer</td>
+                <td>San Francisco</td>
+                <td>$205,500</td>
+            </tr>
+            <tr>
+                <td>Sonya</td>
+                <td>Frost</td>
+                <td>Software Engineer</td>
+                <td>Edinburgh</td>
+                <td>$103,600</td>
+            </tr>
+            <tr>
+                <td>Jena</td>
+                <td>Gaines</td>
+                <td>Office Manager</td>
+                <td>London</td>
+                <td>$90,560</td>
+            </tr>
+            <tr>
+                <td>Quinn</td>
+                <td>Flynn</td>
+                <td>Support Lead</td>
+                <td>Edinburgh</td>
+                <td>$342,000</td>
+            </tr>
+            <tr>
+                <td>Charde</td>
+                <td>Marshall</td>
+                <td>Regional Director</td>
+                <td>San Francisco</td>
+                <td>$470,600</td>
+            </tr>
+            <tr>
+                <td>Haley</td>
+                <td>Kennedy</td>
+                <td>Senior Marketing Designer</td>
+                <td>London</td>
+                <td>$313,500</td>
+            </tr>
+            <tr>
+                <td>Tatyana</td>
+                <td>Fitzpatrick</td>
+                <td>Regional Director</td>
+                <td>London</td>
+                <td>$385,750</td>
+            </tr>
+            <tr>
+                <td>Michael</td>
+                <td>Silva</td>
+                <td>Marketing Designer</td>
+                <td>London</td>
+                <td>$198,500</td>
+            </tr>
+            <tr>
+                <td>Paul</td>
+                <td>Byrd</td>
+                <td>Chief Financial Officer (CFO)</td>
+                <td>New York</td>
+                <td>$725,000</td>
+            </tr>
+            <tr>
+                <td>Gloria</td>
+                <td>Little</td>
+                <td>Systems Administrator</td>
+                <td>New York</td>
+                <td>$237,500</td>
+            </tr>
+            <tr>
+                <td>Bradley</td>
+                <td>Greer</td>
+                <td>Software Engineer</td>
+                <td>London</td>
+                <td>$132,000</td>
+            </tr>
+            <tr>
+                <td>Dai</td>
+                <td>Rios</td>
+                <td>Personnel Lead</td>
+                <td>Edinburgh</td>
+                <td>$217,500</td>
+            </tr>
+            <tr>
+                <td>Jenette</td>
+                <td>Caldwell</td>
+                <td>Development Lead</td>
+                <td>New York</td>
+                <td>$345,000</td>
+            </tr>
+            <tr>
+                <td>Yuri</td>
+                <td>Berry</td>
+                <td>Chief Marketing Officer (CMO)</td>
+                <td>New York</td>
+                <td>$675,000</td>
+
+        </tbody>
+        <tfoot>
+            <tr>
+                <th colspan="4" style="text-align:right">Total:</th>
+                <th></th>
+            </tr>
+        </tfoot>
+    </table>
